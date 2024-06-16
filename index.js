7056f97a32ad3e6e6b20d3f345fbefc01b5183de
VenkataVamsiJonnadula <46235184+VenkataVamsiJonnadula@users.noreply.github.com>
2024-06-16 22:14:54 +0530

    Adding cors support for development

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-'use strict';
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -8,11 +7,19 @@ const libraryRoutes = require('./routes/library-routes');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    const allowedOrigins = ['http://localhost:4200', 'https://your-angular-app.firebaseapp.com'];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 app.use(bodyParser.json());
-
 app.use('/api', libraryRoutes.routes);
-
-
 
 app.listen(config.port, () => console.log('App is listening on url http://localhost:' + config.port));
